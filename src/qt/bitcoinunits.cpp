31557158ffb326bd66b60217e313340fafd5d9ef
sumcash <sumcash@Tys-MacBook-Pro.local>
2022-01-11 06:56:17 -0700

    Name and abbreviations

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -38,9 +38,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
+    case BTC: return QString("SUMC");
     case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case uBTC: return QString::fromUtf8("μSUMC");
     default: return QString("???");
     }
 }
@@ -49,9 +49,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("Sumcashs");
+    case mBTC: return QString("Milli-Sumcashs (1 / 1,000)");
+    case uBTC: return QString("Micro-Sumcashs (1 / 1,000,000)");
     default: return QString("???");
     }
 }
