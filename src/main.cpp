79e77cc2d7f007b32911259fd8ad9918ab8b60a5
sumcash <sumcash@Tys-MacBook-Pro.local>
2022-01-11 08:26:02 -0700

    remove mainnet genesis base

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ CTxMemPool mempool;
 unsigned int nTransactionsUpdated = 0;
 
 map<uint256, CBlockIndex*> mapBlockIndex;
-uint256 hashGenesisBlock("0x12a765e31ffd4059bada1e25190f6e98c99d9714d334efa41a195a7e7e04bfe2");
+uint256 hashGenesisBlock("0x");
 static CBigNum bnProofOfWorkLimit(~uint256(0) >> 20); // Sumcash: starting difficulty is 1 / 2^12
 CBlockIndex* pindexGenesisBlock = NULL;
 int nBestHeight = -1;
