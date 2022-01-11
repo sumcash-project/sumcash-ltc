3458b4e1e24977dfb913dd1b6f067c90490b8525
sumcash <sumcash@Tys-MacBook-Pro.local>
2022-01-11 07:35:04 -0700

    mainnet peermagic

--- src/main.cpp
+++ src/main.cpp
@@ -3079,7 +3079,7 @@ bool static AlreadyHave(const CInv& inv)
 // The message start string is designed to be unlikely to occur in normal data.
 // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
 // a large 4-byte int at any alignment.
-unsigned char pchMessageStart[4] = { 0xfb, 0xc0, 0xb6, 0xdb }; // Sumcash: increase each by adding 2 to bitcoin's value.
+unsigned char pchMessageStart[4] = { 0xfc, 0xc4, 0xba, 0xd2 }; // Sumcash set
 
 
 void static ProcessGetData(CNode* pfrom)
