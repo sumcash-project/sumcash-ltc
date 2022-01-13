e68f7e9554b8738cd219f6ef836e8d9eeb24f2ac
sumcash <sumcash@Tys-MacBook-Pro.local>
2022-01-12 23:03:13 -0700

    v2 set block reward

--- src/main.cpp
+++ src/main.cpp
@@ -1087,7 +1087,7 @@ uint256 static GetOrphanRoot(const CBlockHeader* pblock)
 
 int64 static GetBlockValue(int nHeight, int64 nFees)
 {
-    int64 nSubsidy = 2314.81481 * COIN;
+    int64 nSubsidy = 10000 * COIN;
 
     // Subsidy is cut in half every 840000 blocks, which will occur approximately every 4 years
     nSubsidy >>= (nHeight / 43200); // Sumcash: 43200 is 30 days
